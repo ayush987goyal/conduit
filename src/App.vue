f5d0c2a6efbed60707ce48e486d9e3933c1612f0
Ayush Goyal <ayush987goyal@gmail.com>
2018-04-25 00:29:57 +0530

    added vuetify app and toolbars for auth and no auth

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,25 @@
 <template>
   <v-app>
-    <router-view/>
+    <TheHeader />
+
+    <v-content>
+      <v-container fluid>
+        <router-view></router-view>
+      </v-container>
+    </v-content>
+    
+    <TheFooter />
   </v-app>
 </template>
+
+<script>
+import TheHeader from '@/components/TheHeader';
+import TheFooter from '@/components/TheFooter';
+
+export default {
+  components: {
+    TheHeader,
+    TheFooter
+  }
+};
+</script>
--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import Vuetify from 'vuetify';
 import App from './App.vue';
 import router from './router';
 import store from './store';
-import './registerServiceWorker';
+// import './registerServiceWorker';
 
 import 'vuetify/dist/vuetify.min.css';
 
