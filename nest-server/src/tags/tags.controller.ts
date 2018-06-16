8d1d33316d5312b92b3cb6ff378c910a01400093
Ayush Goyal <ayush987goyal@gmail.com>
2018-06-16 22:36:00 +0530

    Add tags controller and service for getTags

--- nest-server/src/tags/tags.controller.ts
+++ nest-server/src/tags/tags.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { TagsService } from './tags.service';
 
-@Controller('tags')
-export class TagsController {}
+@Controller('api/tags')
+export class TagsController {
+  constructor(private tagsService: TagsService) {}
+
+  @Get()
+  async getTags(): Promise<{ tags: string[] }> {
+    return this.tagsService.getTags();
+  }
+}
--- nest-server/src/tags/tags.service.ts
+++ nest-server/src/tags/tags.service.ts
@@ -1,6 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+
+import { Article } from 'articles/models/article.interface';
 
 @Injectable()
 export class TagsService {
-  constructor() {}
+  constructor(@InjectModel('Article') private readonly articleModel: Model<Article>) {}
+
+  async getTags(): Promise<{ tags: string[] }> {
+    const articles = await this.articleModel.find({}).exec();
+
+    let tags = [].concat(...articles.map(article => article.tagList));
+    tags = [...new Set(tags)];
+
+    return { tags };
+  }
 }
