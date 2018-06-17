16033fbc6d804ebf6ed34d59b0c589507678bf66
Ayush Goyal <ayush987goyal@gmail.com>
2018-06-17 13:42:34 +0530

    add static asests

---
+++ nest-server/public/precache-manifest.08b0c1a0de591120f97f85c752160782.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1e3f4889981cb0c63772",
+    "url": "/js/chunk-vendors.1e3f4889.js"
+  },
+  {
+    "revision": "0c960bcaa7eeb55e4a02",
+    "url": "/js/app.0c960bca.js"
+  },
+  {
+    "revision": "8bb74e5050f766c8d836bd3b18772908",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1e3f4889981cb0c63772",
+    "url": "/css/chunk-vendors.c606b7f2.css"
+  },
+  {
+    "revision": "0c960bcaa7eeb55e4a02",
+    "url": "/css/app.70092602.css"
+  }
+];
